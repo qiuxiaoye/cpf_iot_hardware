8864ede49873ef37645e96986e5ab66d06c87899
Qiu Xiaoye <qiuxiaoye@hotmail.com>
2024-04-15 02:30:04 +0800

    change

--- paho_mqtt/paho_mqtt.py
+++ paho_mqtt/paho_mqtt.py
@@ -27,6 +27,7 @@ def connect_mqtt() -> mqtt_client:
 
     # client = mqtt_client.Client(client_id)
     client = mqtt_client.Client(mqtt_client.CallbackAPIVersion.VERSION1, client_id)
+    
     # client.username_pw_set(username, password)
     client.on_connect = on_connect
     client.connect(broker, port)
