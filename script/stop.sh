d460ef4db2bec98738c116a49ab07b15df7be730
Qiu Xiaoye <qiuxiaoye@hotmail.com>
2025-03-27 19:55:15 +0800

    feat: Enhance stop script to remove log files after stopping the service

--- script/stop.sh
+++ script/stop.sh
@@ -2,3 +2,5 @@
 cd ~/Code/cpf_iot_hardware || exit
 echo "Stopping service at 19:00 work day..."
 /usr/bin/docker compose stop
+
+rm -f /home/CPF-IOT/Code/cpf_iot_hardware/zigbee2mqtt/data/log/*.log
