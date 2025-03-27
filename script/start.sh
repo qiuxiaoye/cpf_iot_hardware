2b0e21bf169258be73cf75a9a7198c55a4da0292
Qiu Xiaoye <qiuxiaoye@hotmail.com>
2025-03-27 19:42:42 +0800

    feat: Update start and stop scripts to generalize service management

--- script/start.sh
+++ script/start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd ~/Code/cpf_iot_hardware || exit
-echo "Starting message_router at 8am every work day..."
-/usr/bin/docker compose start message_router
+echo "Starting service at 8am every work day..."
+/usr/bin/docker compose start
--- script/stop.sh
+++ script/stop.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd ~/Code/cpf_iot_hardware || exit
-echo "Stopping message_router at 19:00 work day..."
-/usr/bin/docker compose stop message_router
+echo "Stopping service at 19:00 work day..."
+/usr/bin/docker compose stop
